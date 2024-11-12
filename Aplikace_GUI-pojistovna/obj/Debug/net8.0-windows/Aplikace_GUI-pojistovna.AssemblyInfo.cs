180eadad18ec30719f5142c12705212d39e3a79a
Z11_falcon <st67053@student.upce.cz>
2024-11-12 16:52:28 +0100

    trash - next time see you in .gitignore

--- Aplikace_GUI-pojistovna/obj/Debug/net8.0-windows/Aplikace_GUI-pojistovna.AssemblyInfo.cs
+++ Aplikace_GUI-pojistovna/obj/Debug/net8.0-windows/Aplikace_GUI-pojistovna.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplikace_GUI-pojistovna")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+738fa4c2a06197775e4fc3ff34d7cdb2e649a103")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df1ae5ab973370bfd6ab80f45f5fffb1e9ad551b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplikace_GUI-pojistovna")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplikace_GUI-pojistovna")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
