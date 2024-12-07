26c60cb5f0d57b40e25ca2518d76fa2243400a9c
Z11_falcon <st67053@student.upce.cz>
2024-12-07 16:00:17 +0100

    cache

--- Aplikace_GUI-pojistovna/obj/Debug/net8.0-windows/Aplikace_GUI-pojistovna.AssemblyInfo.cs
+++ Aplikace_GUI-pojistovna/obj/Debug/net8.0-windows/Aplikace_GUI-pojistovna.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplikace_GUI-pojistovna")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49530537497491dfbe6b232203629d7bb00ae78f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f716b4c1f1ba063d406086e7c752f54c107704fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplikace_GUI-pojistovna")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplikace_GUI-pojistovna")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
