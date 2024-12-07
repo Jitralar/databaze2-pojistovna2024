49530537497491dfbe6b232203629d7bb00ae78f
Z11_falcon <st67053@student.upce.cz>
2024-12-07 15:12:04 +0100

    statistika klientu WIP

--- Aplikace_GUI-pojistovna/SpecificForms/StatistikyKlientu.cs
+++ Aplikace_GUI-pojistovna/SpecificForms/StatistikyKlientu.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+
 
 namespace Aplikace_GUI_pojistovna.SpecificForms
 {
@@ -15,6 +11,31 @@ namespace Aplikace_GUI_pojistovna.SpecificForms
         public StatistikyKlientu()
         {
             InitializeComponent();
+            NaplnDataGridy();
         }
+
+        private void NaplnDataGridy()
+        {
+            DatabaseConnection dbConnection = new DatabaseConnection();
+
+            try
+            {
+                // Načíst data z pohledů
+                var dataKraje = dbConnection.GetDataFromView("stats_rozdeleni_klientu_kraj");
+                var dataPojisteni = dbConnection.GetDataFromView("stats_pocet_klientu_typ_pojistky");
+                var dataRoky = dbConnection.GetDataFromView("stats_pocet_klientu_rok_sjednani");
+
+                // Přiřazení dat do DataGridView
+                dataGridView_stats_rozdeleni_klientu_kraj.DataSource = dataKraje;
+                dataGridView_stats_pocet_klientu_typ_pojistky.DataSource = dataPojisteni;
+                dataGridView_stats_pocet_klientu_rok_sjednani.DataSource = dataRoky;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Chyba při načítání dat: {ex.Message}", "Chyba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
     }
 }
